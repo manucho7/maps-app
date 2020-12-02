3bbd8e06b9505dbf7c1c0385839c65f83bd003c4
manucho <manuel.blanco77@gmail.com>
2020-12-02 10:28:02 -0300

    Extrayendo callback de useEffect(), para agregar marcadores en una funcion aparte

--- src/hooks/useMapBox.js
+++ src/hooks/useMapBox.js
@@ -21,6 +21,23 @@ export const useMapBox = (puntoInicial) => {
     const mapa = useRef();
     const [coords, setCoords] = useState( puntoInicial );
 
+
+    //Funcion para agregar marcadores
+    const agregarMarcador = useCallback( (ev) => {
+        const { lng, lat } = ev.lngLat;
+
+        const marker = new mapboxgl.Marker();
+        marker.id = v4();
+
+        marker
+            .setLngLat([lng, lat])
+            .addTo( mapa.current )
+            .setDraggable(true);
+        
+        marcadores.current[marker.id] = marker;
+    },[]);
+
+
     useEffect(() => {
         const map = new mapboxgl.Map({
             container: mapaDiv.current,
@@ -48,25 +65,13 @@ export const useMapBox = (puntoInicial) => {
 
     //Agregar marcadores cuando hacemos click en mapa
     useEffect(() => {
-        mapa.current?.on('click', (ev) => {
-            const { lng, lat } = ev.lngLat;
-
-            const marker = new mapboxgl.Marker();
-            marker.id = v4();
-
-            marker
-                .setLngLat([lng, lat])
-                .addTo( mapa.current )
-                .setDraggable(true);
-            
-            marcadores.current[marker.id] = marker;
-        });
-
-    }, [])
+        mapa.current?.on('click', agregarMarcador);
+    }, [agregarMarcador]);
 
     return {
         coords,
         setRef,
-        marcadores
+        marcadores,
+        agregarMarcador
     }
 }
